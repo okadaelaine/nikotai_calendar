3fa0b78dd07877ea17910fdfda4b6bb081c026e2
Elaine Okada <3l41n3@gmail.com>
2018-09-27 01:00:56 +0900

    gera dicionario

--- core/cal.py
+++ core/cal.py
@@ -1,10 +1,35 @@
-from datetime import date
+import datetime
 
-initial_date = date(2018, 9, 2)
+"""
+PRIMEIRO DIA DE B em outubro:
+
+2018/10/02
+
+"""
+
+
+# def schedule(person, day, shift):
+#     initial_date = date(2018, 10, 2)
+#     initial_date_toordinal = initial_date.toordinal()
+#     end_date = initial_date_toordinal + 30
+#     for i in range(initial_date_toordinal, end_date):
+#         if i % 6 in (0, 5):
+#             print(date.fromordinal(i), 'weekend')
+#         else:
+#             print(date.fromordinal(i))
+
+"""
+AREA DE TESTES ABAIXO:
+"""
+
+initial_date = datetime.date(2018, 10, 2)
 initial_date_toordinal = initial_date.toordinal()
 end_date = initial_date_toordinal + 30
+
+date_range = {}
 for i in range(initial_date_toordinal, end_date):
     if i % 6 in (0, 5):
-        print(date.fromordinal(i), 'weekend')
+        date_range.update({datetime.date.fromordinal(i).isoformat(): 'weekend'})
     else:
-        print(date.fromordinal(i))
+        date_range.update({datetime.date.fromordinal(i).isoformat(): 'weekday'})
+

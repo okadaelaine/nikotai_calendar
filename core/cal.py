bf87fcac4ee656756741f072bffd69aa254a3c24
Elaine Okada <3l41n3@gmail.com>
2018-09-28 00:36:17 +0900

    refatora e adiciona informacao de turno

--- core/cal.py
+++ core/cal.py
@@ -22,14 +22,28 @@ PRIMEIRO DIA DE B em outubro:
 AREA DE TESTES ABAIXO:
 """
 
-initial_date = datetime.date(2018, 10, 2)
+initial_date = datetime.date(2018, 9, 8)
 initial_date_toordinal = initial_date.toordinal()
-end_date = initial_date_toordinal + 30
+end_date = datetime.date(2018, 12, 31)
+end_date_toordinal = end_date.toordinal()
+"""
+*  =  DIA DE FOLGA
+A  =  TURNO A
+B  =  TURNO B
 
+
+Estou partindo do primeiro dia do turno B 
+para o mes de setembro (2018/09/08) 
+e colocarei o calendario ate o fim de dezembro
+
+"""
 date_range = {}
-for i in range(initial_date_toordinal, end_date):
+for i in range(initial_date_toordinal, end_date_toordinal + 1): #+1 para pegar o ultimo elemento (dia 31 de dezmebro)
     if i % 6 in (0, 5):
-        date_range.update({datetime.date.fromordinal(i).isoformat(): 'weekend'})
+        date_range.update({datetime.date.fromordinal(i).isoformat(): '*'})
     else:
-        date_range.update({datetime.date.fromordinal(i).isoformat(): 'weekday'})
+        if i % 12 in (1, 2, 3, 4):
+            date_range.update({datetime.date.fromordinal(i).isoformat(): 'B'}) #pega os primeiros dias de trabalho e coloca como B
+        else:
+            date_range.update({datetime.date.fromordinal(i).isoformat(): 'A'})
 
